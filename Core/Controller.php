e68d67969e8f3d73fb5c959ced26998ac554d218
lukehayes <lukepurplehayes@gmail.com>
2019-04-27 17:50:08 +0100

    Added View::render()

--- Core/Controller.php
+++ Core/Controller.php
@@ -7,4 +7,13 @@ namespace Core;
 class Controller
 {
 	public function __construct() {}
+
+	/**
+	 * Render a view
+	 * @param  $view $view The view to be rendered
+	 * @return void
+	 */
+	public function render( $view ) {
+		// TODO
+	}
 }
