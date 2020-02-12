c428674cd16d3799c78627b6a5af6f0b328cbf4a
lukehayes <lukepurplehayes@gmail.com>
2020-02-12 09:55:44 +0000

    Added Core\Router class

---
+++ Core/Router.php
@@ -0,0 +1,47 @@
+<?php
+namespace Core;
+
+class Router {
+
+    private $routes = [];
+
+    public function __construct(){}
+
+    /**
+     * Add a get route to the router
+     *
+     * @param $path The endpoint for a GET request
+     * @param $controller The name of the controller
+     *
+     * @return void
+     */
+    public function get($path, $controller) : void {
+        $route = [$path => $controller];
+        $this->routes['get'][] = $route;
+    }
+
+    /**
+     * Add a POST route to the router
+     *
+     * @param $path The endpoint for a POST request
+     * @param $controller The name of the controller
+     *
+     * @return void
+     */
+    public function post($path, $controller) : void {
+        $route = [$path => $controller];
+        $this->routes['post'][] = $route;
+    }
+
+    /**
+     * Routes array getter
+     *
+     * @return array
+     */
+    public function getRoutes() : array {
+        return $this->routes;
+    }
+
+}
+
+?>
