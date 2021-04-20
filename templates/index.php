99696c3fb4f858cb1f3784de4d59c5ac65f1765c
lukehayes <lukepurplehayes@gmail.com>
2021-04-20 16:26:41 +0100

    Added templates/index.php test template file.

---
+++ templates/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <title>CMS</title>
+    </head>
+    <body>
+        <h4>Loaded</h4>
+    </body>
+</html>
