ee8970452efdcc8457f17cbbc718b9c053f3fc72
lukehayes <lukepurplehayes@gmail.com>
2021-04-20 17:24:01 +0100

    Moved header content into base-header partial.

--- templates/index.php
+++ templates/index.php
@@ -1,11 +1,1 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width">
-        <title>CMS</title>
-    </head>
-    <body>
-        <h4>Loaded</h4>
-    </body>
-</html>
+<h4>Index.</h4>
