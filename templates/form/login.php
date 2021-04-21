df4691e678439f6b1c7c8d6dc84762f675569e1e
lukehayes <lukepurplehayes@gmail.com>
2021-04-21 16:55:13 +0100

    Added form/login template partials.

---
+++ templates/form/login.php
@@ -0,0 +1,7 @@
+
+<form action="/login" method="post">
+    <input type="text" name="username" id="" value="">
+    <input type="password" name="password" id="" value="">
+    
+    <button type="submit">Login</button>
+</form>
