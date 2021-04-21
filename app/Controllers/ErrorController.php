183314d69ef4c981a72556a514d19452cb01d662
lukehayes <lukepurplehayes@gmail.com>
2021-04-21 14:06:26 +0100

    ErrorController::index() now sets the 'title' variable from action method.

--- app/Controllers/ErrorController.php
+++ app/Controllers/ErrorController.php
@@ -10,6 +10,7 @@ class ErrorController extends Controller
 {
     public function index(string $error="404.")
     {
-        $this->view->render("error/404");
+        $this->data['title'] = "Error: 404.";
+        $this->view->render("error/404", array_merge($this->data));
     }
 }
