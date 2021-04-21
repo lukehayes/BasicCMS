73c3bdd2e8fd941cda10930113166873ac0b3bee
lukehayes <lukepurplehayes@gmail.com>
2021-04-21 16:53:43 +0100

    SiteController actions now display a name variable.

--- app/Controllers/SiteController.php
+++ app/Controllers/SiteController.php
@@ -8,8 +8,22 @@ use Core\Controller;
  */
 class SiteController extends Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
     public function index()
     {
-        $this->view->render("index");
+        $data['name'] = "Index";
+
+        $this->view->render("index", array_merge($this->data, $data));
+    }
+
+    public function hello()
+    {
+        $data['name'] = "Hello";
+
+        $this->view->render("index", array_merge($this->data, $data));
     }
 }
