fe3a11e7ebd17f959c115c708381c4c26faf3dc0
Luke <lukepurplehayes@gmail.com>
2021-04-22 21:45:58 +0100

    index.php now calls app->run().

--- index.php
+++ index.php
@@ -4,12 +4,5 @@ require 'vendor/autoload.php';
 use Core\App;
 use Core\Router;
 
-$r = new Router();
-
-$r->get('/hello', 'SiteController','index');
-$r->get('/other', 'SiteController','other');
-$r->post('/other', 'SiteController','other');
-
-dump($r->routes());
-
-
+$app = new App(new Router);
+$app->run();
